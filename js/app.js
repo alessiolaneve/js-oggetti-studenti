4e635fbe508a4aec7e136b707a072a19f5620322
alessiolaneve <alessioln98@gmail.com>
2024-03-25 17:09:53 +0100

    milestones completed

--- js/app.js
+++ js/app.js
@@ -23,3 +23,31 @@ console.log(studenti)
 studenti.forEach(function(studente) {
     console.log(studente.nome + ' ' + studente.cognome)
 });
+
+
+// Dare la possibilità all’utente, attraverso 3 prompt(),
+// di aggiungere un nuovo oggetto studente inserendo nell’ordine: nome, cognome e età.
+// Aggiungere il nuovo studente all’array di studenti
+// Ciclare su tutti gli studenti e stampare per ognuno di essi
+// tutte le proprietà: nome, cognome, eta
+// prima ciclate con un ciclo for
+// poi commentate il ciclo for e usate il forEach
+
+
+const nuovoStudente =  {
+    nome:prompt('Inserisci il nome del nuovo studente.'), 
+    cognome:prompt('Inserisci il cognome del nuovo studente.'), 
+    eta:prompt('Inserisci gli anni del nuovo studente.'),
+}
+console.log(nuovoStudente)
+studenti.push(nuovoStudente) 
+console.log(studenti)
+
+// for (let i = 0; i < studenti.length; i++) {
+//     const studente = studenti[i];
+//     console.log(`${studente.nome} `+`${studente.cognome} `+`${studente.eta}`)
+// }
+
+studenti.forEach(function(studente) {
+    console.log(`${studente.nome} `+`${studente.cognome} `+`${studente.eta}`)
+});
